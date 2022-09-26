cd18fd2382e920bd481b73d8cba3b6e123e93d3f
Pornsawan Janpeng <leejay.skz8@gmail.com>
2022-09-26 09:20:54 +0700

    Add project files.

---
+++ Lab6_game1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab6_game1
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
